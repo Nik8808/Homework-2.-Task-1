a4df38673355338dc90cb8f8d26cbb46c0b23fb2
Nikita <inik8808@gmail.com>
2023-04-05 15:07:01 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+        int bonusRuble = 20;
+        int bonus = ticketPrice / bonusRuble;
+        System.out.println("Количество бонусов:");
+        System.out.println(bonus);
+    }
+}
